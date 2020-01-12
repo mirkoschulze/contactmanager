8fb9bb54f6dffb679f9c4dc88fb8c2950b677a64
AVATARMKVII <avatarmkvii@hotmail.com>
2020-01-12 02:38:10 +0100

    clean web
    removed defaul xhtml pages and classes now in api

--- contactmanager-web/src/main/java/de/mcdb/contactmanagerweb/Controller.java
+++ contactmanager-web/src/main/java/de/mcdb/contactmanagerweb/Controller.java
@@ -1,5 +1,6 @@
 package de.mcdb.contactmanagerweb;
 
+import de.mcdb.contactmanagerapi.Dao;
 import de.mcdb.contactmanagerapi.datamodel.Company;
 import de.mcdb.contactmanagerapi.datamodel.Division;
 import de.mcdb.contactmanagerapi.datamodel.Staffer;
@@ -29,10 +30,31 @@ public class Controller implements Serializable {
     private Division division;
     private Staffer staffer;
 
-    private List<Company> companies;
-    private List<Division> divisions;
-    private List<Staffer> staffers;
+    private List<Company> companies = new ArrayList<>();
+    private List<Division> divisions = new ArrayList<>();
+    private List<Staffer> staffers = new ArrayList<>();
 
+//    private Dao dao = new Dao();
+    @PostConstruct
+    public void init() {
+        Company genCompany = new Company("Firma 1");
+        Division genDivision = new Division("Abteilung 1", genCompany);
+        Staffer genStaffer = new Staffer("Vorname 1", "Nachname 1", genDivision);
+
+        this.companies.add(genCompany);
+        this.divisions.add(genDivision);
+        this.staffers.add(genStaffer);
+
+        genCompany = new Company("Firma 2");
+        genDivision = new Division("Abteilung 2", genCompany);
+        genStaffer = new Staffer("Vorname 2", "Nachname 2", genDivision);
+
+        this.companies.add(genCompany);
+        this.divisions.add(genDivision);
+        this.staffers.add(genStaffer);
+    }
+
+    //<editor-fold defaultstate="collapsed" desc="Getter / Setter">
     public Company getCompany() {
         return company;
     }
@@ -80,13 +102,5 @@ public class Controller implements Serializable {
     public void setStaffers(List<Staffer> staffers) {
         this.staffers = staffers;
     }
-
-    @PostConstruct
-    public void init() {
-        this.company = new Company("Firma");
-        this.division = new Division("Abteilung", this.company);
-
-        this.companies = new ArrayList<>();
-        this.companies.add(this.company);
-    }
+    //</editor-fold>
 }
